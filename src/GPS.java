0de56ec194d1a9d0d8c81c37839d2401b7476f55
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2010-11-13 19:29:57 -0800

    Better formatting of the GPS output
    - Updated API.jar library to 2.6
    - Fixed Orientation output so 0 degrees is north.
    - Added 3-letter precision compass headings to orientation output
    - Added "m" label to altitude
    - Added north/south labels to latitude, and made it always positive
    - Added east/ewst labels to longitude and made it always positive
    - Formatted the output so it'll (hopefully) always be a fixed width
    - Added color to the output, so data values are (hopefully) easily
    	recognizable.

--- src/GPS.java
+++ src/GPS.java
@@ -51,22 +51,87 @@ public class GPS extends Mod {
 		String[] tokens = command.split(" ");
 		return this.parseCommand(player, tokens, isAdmin);
 	}
+
+	protected String generateOrientation(Float degrees) {
+		degrees = (degrees+270)%360;
+		String textDirection = "";
+
+		// Determine North/South facing.
+		if( degrees <= 78.75 || degrees > 281.25 ) {
+			textDirection = "N";
+		} else if( degrees > 101.25 && degrees <= 258.75 ) {
+			textDirection = "S";
+		}
+
+		// Determine East/West facing.
+		if (degrees > 11.25 && degrees <= 168.75 ) {
+			textDirection += "E";
+		} else if( degrees > 190.25 && degrees <= 348.75 ) {
+			textDirection += "W";
+		}
+
+		// Determine Additional north/east/south/west facing for 3-letter precision.
+		if( (degrees > 326.25 || degrees <= 33.75) && !textDirection.equalsIgnoreCase("N") ) {
+			textDirection = "N"+textDirection;
+		} else if( (degrees > 56.25 && degrees <= 123.75) && !textDirection.equalsIgnoreCase("E") ) {
+			textDirection = "E"+textDirection;
+		} else if( (degrees > 146.25 && degrees <= 213.75) && !textDirection.equalsIgnoreCase("S") ) {
+			textDirection = "S"+textDirection;
+		} else if( (degrees > 236.25 && degrees <= 303.75) && !textDirection.equalsIgnoreCase("W") ) {
+			textDirection = "W"+textDirection;
+		}
+		
+
+		String directionPadding = "";
+		if( textDirection.length() < 2 ) directionPadding += '_';
+		if( textDirection.length() < 3 ) directionPadding += '_';
+		
+		return String.format("\u00A77(%s\u00A7d%s\u00A77) %s\u00A7d%.1f\u00A7f",
+			directionPadding, textDirection,
+			this.getDegreesPadding((double)degrees), degrees
+		);
+	}
+
+	protected String generateLatitude(Double degrees) {
+		return String.format("\u00A77%s\u00A7a%.1f %s\u00A7f",
+			this.getDegreesPadding(Math.abs(degrees)), Math.abs(degrees),
+			(degrees<0?"N":degrees>0?"S":"")
+		);
+	}
+	protected String generateLongitude(Double degrees) {
+		return String.format("\u00A77%s\u00A7a%.1f %s\u00A7f",
+			this.getDegreesPadding(Math.abs(degrees)), Math.abs(degrees),
+			(degrees<0?"E":degrees>0?"W":"")
+		);
+	}
+	protected String generateAltitude(Double altitude) {
+		return String.format("\u00A77%s\u00A79%03.0f\u00A77m\u00A7f",
+			this.getDegreesPadding(altitude), altitude);
+	}
+
+	protected String getDegreesPadding(Double degrees) {
+		String padding = "";
+		if( degrees < 100 ) padding += "0";
+		if( degrees < 10 ) padding += "0";
+		return padding;
+	}
 	
 	protected void getCoordinates(Player player, Player target) {
 		Location loc = target.getLocation();
-		Double latitude = loc.getX();
-		Double longitude = loc.getZ();
 		Double altitude = loc.getY();
-		Float orientation = target.getRotation();
 
 		String playerLabel = "";
 		if( player.getId() != target.getId() ) {
-			playerLabel = String.format("%s: ", target.getName());
+			playerLabel = String.format("\u00A77%s:\u00A7f ", target.getName());
 		}
 
 		player.sendChat(String.format(
-			"%sPosition: %.1f,%.1f; Altitude: %.0f; Orientation: %.1f",
-			playerLabel, latitude, longitude, altitude, orientation)
-		);
+			"%s\u00A77Loc:[ %s\u00A77, %s\u00A77 ] Facing:[ %s\u00A77 ] Alt:[ %s\u00A77 ]",
+			playerLabel,
+			this.generateLatitude(loc.getX()),
+			this.generateLongitude(loc.getZ()),
+			this.generateOrientation(target.getRotation()),
+			this.generateAltitude(loc.getY())
+		));
 	}
 }
